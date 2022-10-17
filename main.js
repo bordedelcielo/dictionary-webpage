c4d19519c7e47062c76479cffbf44a5c2625d57a
bordedelcielo <christopher.thrutchley@gmail.com>
2022-10-16 22:35:06 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('Are you ready to rock & roll?');
+
+console.log(start)
+
+function clickButton() {
+        console.log('You just clicked the button')
+}
+
+start.onclick = (event) => {
+    clickButton()
+}
